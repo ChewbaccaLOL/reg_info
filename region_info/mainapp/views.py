508a9a1b7f0792c7b31f4cc47793affc7581d71a
ChewbaccaLOL <apocalypseman99@gmail.com>
2020-05-04 02:31:41 +0300

    Created basic pages

--- region_info/mainapp/views.py
+++ region_info/mainapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'ChewbaccaLOL',
+        'title': 'Article Post 1',
+        'content':'First article',
+        'date_posted': '02.05.2020'
+    },
+    {
+        'author': 'admin001',
+        'title': 'Article Post 2',
+        'content':'Second article',
+        'date_posted': '03.05.2020'
+    }
+]
+
+def home(request):
+    context={
+        'posts': posts
+    }
+    return render(request, 'infoservice/home.html', context)
+
+def about(request):
+    return render(request, 'infoservice/about.html', {'title':'About'})
+
+
